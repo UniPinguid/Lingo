e71519412aa1d82794053029255c8dee48fcf6e2
Nguyễn Hữu Khôi <65937294+khoihuu0301@users.noreply.github.com>
2023-07-12 19:20:33 +0700

    Update settings.py

--- lingo/lingo/settings.py
+++ lingo/lingo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--_8#$2@b7h7z*u=+zw34n5*xv+mctmqm(kups#sca!cbpl+x4m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['57.128.165.116']
 
 
 # Application definition
